e4fad87373c1434a07f90d730270e5549e74d2b9
Dudziej <dudzik32@gmail.com>
2021-01-11 13:41:55 +0100

    ZAD2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
@@ -5,6 +7,14 @@ function App() {
  for(let i=0; i<11; i++){
     array.push(<li>{Math.floor(Math.random() * 100)}</li>)
   }
-  return(<ul>{array}</ul>)
+
+  const refreshPage = ()=>{
+     window.location.reload();
+  }
+  return(<div>
+        <ul>{array}</ul>
+        <button onClick={refreshPage}>Refresh</button>
+        </div>);
+    
 }
 export default App;
